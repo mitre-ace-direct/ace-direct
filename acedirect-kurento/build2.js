0d99c52ca1331749d49c63906d6e642f05e1dcd6
Ed Torres <ertorres@mitre.org>
2021-02-18 23:49:06 +0000

    signaling server now uses dat config.json for all config vals; build process generates WebRtcEndpoint.conf.ini on the fly

--- acedirect-kurento/build2.js
+++ acedirect-kurento/build2.js
@@ -81,14 +81,11 @@ function execCommand(cmd,wdir) {
 
 async function go() {
 
-  s = await execCommand('rm -rf node_modules >/dev/null  # removing node_modules','.');
   s = await execCommand('rm -rf vendor/kurento-client-js/node_modules >/dev/null  # removing node_modules','.');
   s = await execCommand('rm -rf vendor/kurento-jsonrpc/node_modules >/dev/null  # removing node_modules','.');
   s = await execCommand('rm -rf vendor/reconnect-ws/node_modules >/dev/null  # removing node_modules','.');
   s = await execCommand('sleep 5  # pause before npm install... ','.');
   s = await execCommand('which yarn >/dev/null  ','.');
-
-  s = await execCommand('npm install   # main install','.');
 
   s = await execCommand('cp confs/jssip-modifications/RTCSession.js node_modules/jssip/lib-es5/.', '.');
   s = await execCommand('cp confs/jssip-modifications/UA.js node_modules/jssip/lib-es5/.', '.');
