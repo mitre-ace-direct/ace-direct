843f7d569a55d9391409c8a8fd19b9f750a8c6a8
Ed Torres <ertorres@mitre.org>
2021-02-18 15:18:05 +0000

    initial commit

---
+++ managementportal/build.js
@@ -0,0 +1,190 @@
+const path = require('path');
+var fs = require('fs');
+
+
+const CSS = [
+    //admin-lte
+    // 'admin-lte/bootstrap/css/bootstrap.min.css',
+    // 'admin-lte/dist/css/AdminLTE.min.css',
+    'admin-lte/dist/css/adminlte.min.css',
+    'admin-lte/dist/css/adminlte.min.css.map',
+    // 'admin-lte/dist/css/skins/_all-skins.min.css',
+    'admin-lte/plugins/select2/css/select2.min.css',
+    //angular-datatables
+    'angular-datatables/dist/css/angular-datatables.min.css',
+    //bootstrap date range picker
+    'bootstrap-daterangepicker/daterangepicker.css',
+    //bootstrap-select
+    'bootstrap-select/dist/css/bootstrap-select.min.css',
+    //datatables
+    'datatables/media/css/jquery.dataTables.min.css',
+    //font-awesome (from admin-lte)
+    'admin-lte/plugins/fontawesome-free/css/all.min.css',
+    //ionicons
+    'ionicons/dist/css/ionicons.min.css',
+    //izitoast
+    'izitoast/dist/css/iziToast.min.css',
+
+];
+const FONT = [
+    // 'font-awesome/fonts/fontawesome-webfont.woff2',
+    //font awesome fonts
+    'ionicons/dist/fonts/ionicons.woff',
+    'ionicons/dist/fonts/ionicons.woff2',
+    'ionicons/dist/fonts/ionicons.ttf'
+];
+// separate some of the fonts since they look in the web fonts directory for them
+const WEB_FONT = [
+    'admin-lte/plugins/fontawesome-free/webfonts/fa-solid-900.woff',
+    'admin-lte/plugins/fontawesome-free/webfonts/fa-solid-900.woff2',
+];
+
+const JS = [
+    //admin lte
+    // 'admin-lte/plugins/flot/jquery.flot.min.js',
+    // 'admin-lte/plugins/flot/jquery.flot.categories.min.js',
+    // 'admin-lte/plugins/flot/jquery.flot.time.min.js',
+    // 'admin-lte/plugins/flot/jquery.flot.pie.min.js',
+    // 'admin-lte/plugins/flot/jquery.flot.resize.min.js',
+    // 'admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+    'admin-lte/plugins/jquery/jquery.min.js',
+    'admin-lte/plugins/select2/js/select2.min.js',
+    // 'admin-lte/dist/js/app.min.js',
+    'admin-lte/dist/js/adminlte.min.js',
+    'admin-lte/dist/js/adminlte.min.js.map',
+    //angular
+    'angular/angular.min.js',
+    'angular/angular.min.js.map',
+    //angular datatables
+    'angular-datatables/dist/angular-datatables.min.js',
+    //angular-ui-bootstrap
+    'angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+    //angular-moment-duration-format
+    'angular-moment-duration-format/angular-moment-duration-format.min.js',
+    //angular-route
+    'angular-route/angular-route.min.js',
+    'angular-route/angular-route.min.js.map',
+    //bootstrap (from admin-lte)
+    'admin-lte/plugins/bootstrap/js/bootstrap.min.js',
+    'admin-lte/plugins/bootstrap/js/bootstrap.min.js.map',
+    //bootstrap date range picker
+    'bootstrap-daterangepicker/daterangepicker.js',
+    //bootstrap-select
+    'bootstrap-select/dist/js/bootstrap-select.min.js',
+    //datatables.net
+    'datatables.net/js/jquery.dataTables.min.js',
+    //jquery-flot
+    'flot/lib/globalize.js',
+    'flot/source/jquery.flot.js',
+    'flot/source/jquery.canvaswrapper.js',
+    'flot/source/jquery.colorhelpers.js',
+    'flot/source/jquery.flot.categories.js',
+    'flot/source/jquery.flot.time.js',
+    'flot/source/jquery.flot.pie.js',
+    'flot/source/jquery.flot.resize.js',
+    'flot/source/jquery.flot.saturated.js',
+    'flot/source/jquery.flot.browser.js',
+    'flot/source/jquery.flot.drawSeries.js',
+    'flot/source/jquery.flot.uiConstants.js',
+    'flot/source/jquery.flot.symbol.js',
+    'flot/source/jquery.flot.legend.js',
+
+    //izitoast
+    'izitoast/dist/js/iziToast.min.js',
+    //flot-axislabels
+    'flot-axislabels/jquery.flot.axislabels.js',
+    //moment-duration-format
+    'moment-duration-format/lib/moment-duration-format.js',
+    //moment
+    'moment/min/moment.min.js',
+    'moment/min/moment.min.js.map',
+    //popper
+    'admin-lte/plugins/popper/umd/popper.min.js',
+    'admin-lte/plugins/popper/umd/popper.min.js.map',
+    //bootstrap-select
+    'bootstrap-select/dist/js/bootstrap-select.min.js.map',
+
+];
+const IMAGES = [
+    'datatables/media/images/sort_asc.png',
+    'datatables/media/images/sort_desc.png',
+    'datatables/media/images/sort_both.png'
+];
+
+
+if (!fs.existsSync('./public/assets')) {
+    fs.mkdirSync('./public/assets');
+}
+if (!fs.existsSync('./public/assets/js')) {
+    fs.mkdirSync('./public/assets/js');
+}
+if (!fs.existsSync('./public/assets/css')) {
+    fs.mkdirSync('./public/assets/css');
+}
+if (!fs.existsSync('./public/assets/fonts')) {
+    fs.mkdirSync('./public/assets/fonts');
+}
+if (!fs.existsSync('./public/assets/webfonts')) {
+    fs.mkdirSync('./public/assets/webfonts');
+}
+if (!fs.existsSync('./public/assets/images')) {
+    fs.mkdirSync('./public/assets/images');
+}
+JS.map(asset => {
+    let filename = asset.substring(asset.lastIndexOf("/") + 1);
+    let from = path.resolve(__dirname, `./node_modules/${asset}`);
+    let to = path.resolve(__dirname, `./public/assets/js/${filename}`);
+    if (fs.existsSync(from)) {
+        fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    } else {
+        console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+        process.exit(1);
+    }
+});
+
+CSS.map(asset => {
+    let filename = asset.substring(asset.lastIndexOf("/") + 1);
+    let from = path.resolve(__dirname, `./node_modules/${asset}`);
+    let to = path.resolve(__dirname, `./public/assets/css/${filename}`);
+    if (fs.existsSync(from)) {
+        fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    } else {
+        console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+        process.exit(1);
+    }
+});
+
+FONT.map(asset => {
+    let filename = asset.substring(asset.lastIndexOf("/") + 1);
+    let from = path.resolve(__dirname, `./node_modules/${asset}`);
+    let to = path.resolve(__dirname, `./public/assets/fonts/${filename}`);
+    if (fs.existsSync(from)) {
+        fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    } else {
+        console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+        process.exit(1);
+    }
+});
+WEB_FONT.map(asset => {
+    let filename = asset.substring(asset.lastIndexOf("/") + 1);
+    let from = path.resolve(__dirname, `./node_modules/${asset}`);
+    let to = path.resolve(__dirname, `./public/assets/webfonts/${filename}`);
+    if (fs.existsSync(from)) {
+        fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    } else {
+        console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+        process.exit(1);
+    }
+});
+
+IMAGES.map(asset => {
+    let filename = asset.substring(asset.lastIndexOf("/") + 1);
+    let from = path.resolve(__dirname, `./node_modules/${asset}`);
+    let to = path.resolve(__dirname, `./public/assets/images/${filename}`);
+    if (fs.existsSync(from)) {
+        fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    } else {
+        console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+        process.exit(1);
+    }
+});
---
+++ managementportal/public/js/controllers/dashboard.js
@@ -0,0 +1,206 @@
+'use strict';
+
+var dbController = angular.module('dashboardModule', ['csrService', 'angularDurationFormat'])
+		.controller('dashboardController', function($scope, $http, $window, socket) {
+
+		$scope.Queues = [];
+		$scope.qNames = [];
+		$scope.queue = "";
+		$scope.Agents = [];
+		$scope.summary = {};
+		$scope.summary.callers = 0;
+		$scope.summary.completed = 0;
+		$scope.summary.holdtime = 0;
+		$scope.summary.abandoned = 0;
+		$scope.summary.avgholdtime = 0;
+
+		// receives queue summary update every minute
+		socket.on('queue', function (data){
+			$scope.Queues = data.queues;
+			if (data.queues.length !== $scope.qNames.length) {
+				$scope.qNames = [];
+				for (var i=0; i<data.queues.length; i++) {
+					$scope.qNames.push(data.queues[i].queue);
+				}
+			}
+		});
+
+		function findAgent(scopeagents, dataagent) {
+			for (var i=0; i<scopeagents.length; i++) {
+				if (scopeagents[i].agent === dataagent.agent)
+					return scopeagents[i];
+			}
+			return null;
+		}
+
+		socket.on('agent-resp', function (data){
+			if (data.agents) {
+				for (var i=0; i<data.agents.length; i++) {
+					var a = findAgent($scope.Agents, data.agents[i]);
+					if (a) {
+						for (var prop in data.agents[i]) {
+							a[prop] = data.agents[i][prop];
+						}
+					}
+					else {
+						$scope.Agents.push(data.agents[i]);
+					}
+				}
+
+				updateAgentStatusPieChart(data.agents);
+			}
+		});
+
+		socket.on('queue-resp', function (data){
+			$scope.Queues = data.queues;
+			calculateSummary();
+		});
+
+		socket.on('sipconf', function (data){
+			$window.sipconf = data; // so the data can be accessed by non-angular javascript under the window element
+		});
+		socket.on('queueconf', function (data){
+			$window.queueconf = data;
+		});
+
+		socket.on('agent-request', function (data){
+			console.log("Received agent-request help data..." + JSON.stringify(data, null,2,true));
+
+			for (var i=0; i<$scope.Agents.length; i++) {
+				if ($scope.Agents[i].agent === data) {
+					console.log("Extension needs assistance: " + $scope.Agents[i].agent);
+					$scope.Agents[i].help = 'yes';
+				}
+			}
+		});
+
+		var calculateSummary = function () {
+			//initialize summary to zero since we are going to sum across all queues
+			$scope.summary.callers = 0;
+			$scope.summary.completed = 0;
+			$scope.summary.abandoned = 0;
+			let totalHoldTime = 0;
+			for (var i=0; i<$scope.Queues.length; i++) {
+				$scope.summary.callers += Number($scope.Queues[i].callers);
+
+				$scope.summary.completed += $scope.Queues[i].completed;
+
+				$scope.summary.abandoned += $scope.Queues[i].abandoned;
+
+				totalHoldTime += Number($scope.Queues[i].cumulativeHoldTime);
+			}
+
+			if ($scope.summary.completed > 0 && totalHoldTime > 0) {
+				$scope.summary.avgholdtime = Number((totalHoldTime / $scope.summary.completed)/60).toFixed(2);
+			}
+			else {
+				$scope.summary.avgholdtime = 0;
+			}
+		};
+
+		$scope.initData = function () {
+			   socket.emit('config', 'agent');
+			   socket.emit('config', 'webuser');
+			   socket.emit('ami-req', "agent");
+			   socket.emit('ami-req', "queue");
+		};
+
+		angular.element(document).ready($scope.initData());
+	});
+
+function updateAgentStatusPieChart(agents) {
+	var temp = agents.reduce(function(p, c){
+		var defaultValue = {
+		  status: c.status,
+		  data: 0
+		};
+		p[c.status] = p[c.status] || defaultValue;
+		p[c.status].data++;
+
+		return p;
+	  }, {});
+
+	var agentStatusSummary = [];
+	for( var k in temp ){
+		agentStatusSummary.push(temp[k]);
+	}
+
+	agentStatusSummary.forEach(function(e) {
+		e.label = e.status;
+		delete e.status;
+		//handle color logic for pie charts
+		switch(e.label) {
+			case 'Logged Out':
+				e.color = '#d3d3d3';
+				break;
+			case 'In Call':
+				e.color = '#d9534f';
+				break;
+			case 'Ready':
+				e.color = '#5cb85c';
+				break;
+			case 'Away':
+				e.color = '#f4f470';
+				break;
+		}
+	});
+
+	$.plot("#agentStatusPieChart", agentStatusSummary, {
+		series: {
+			pie: {
+				show: true,
+				radius: 1,
+				label: {
+					show: true,
+					radius: 0.6,
+					formatter: labelFormatter,
+					background: {
+						opacity: 0.5
+					}
+				}
+			}
+		},
+		legend: {
+			show: false
+		}
+	});
+}
+
+dbController.directive('highlightOnChange', function() {
+	  return {
+	    link : function(scope, element, attrs) {
+	      var timer;
+	      attrs.$observe( 'highlightOnChange', function ( val ) {
+	        if (val === 'yes') {
+	        	timer = setInterval(function () {
+	        		jQuery(element).addClass("agent-assistance");
+	            	jQuery(element).fadeOut(500, function() {
+	            		jQuery(element).fadeIn(1500);
+	                });
+	            }, 2000);
+	        }
+	        else {
+	        	if (timer) clearInterval(timer);
+	        	jQuery(element).removeClass("agent-assistance");
+	        }
+	      });
+	    }
+	  };
+	});
+
+dbController.filter('shownum', function () {
+	return function (input) {
+		if (!input) {
+			return 0;
+		}
+		else {
+			return input;
+		}
+	};
+});
+
+dbController.filter('minsectimeformat', function () {
+	return function (input) {
+		return moment.duration(Number(input), "minutes").format('mm:ss', { trim: false });
+	};
+});
