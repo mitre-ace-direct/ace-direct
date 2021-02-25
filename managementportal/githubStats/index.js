0fdfe77ca3a128893ede9018c0cbc99db54bf39c
Paul Jacyk <pjacyk@dev3demo.task3acrdemo.com>
2021-02-25 16:59:19 +0000

    Changes for first pass through managementportal with eslint. More to do later.

--- managementportal/githubStats/index.js
+++ managementportal/githubStats/index.js
@@ -1,12 +1,11 @@
-'use strict';
-var express = require('express'),
-    mysql = require('mysql'),
-    https = require('https'),
-    github = require('octonode'),
-    request = require('request');
+const express = require('express');
+const mysql = require('mysql');
+const https = require('https');
+const github = require('octonode');
+const request = require('request');
 
-request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+request('http://www.google.com', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
 });
