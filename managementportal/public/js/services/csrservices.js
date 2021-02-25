11dc6564fd4ca41ae16b6d9ff38e3ebbe708a4dc
Paul Jacyk <pjacyk@dev3demo.task3acrdemo.com>
2021-02-25 16:59:19 +0000

    Changes for first pass through managementportal with eslint. More to do later.

--- managementportal/public/js/services/csrservices.js
+++ managementportal/public/js/services/csrservices.js
@@ -1,50 +1,47 @@
 /**
  * New node file
  */
-'use strict';
 
-angular.module('csrService', []).factory('socket', ['$rootScope', '$http', '$timeout', '$q', function ($rootScope, $http, $timeout, $q) {
-    var socket = io.connect('https://' + window.location.host, {
-        path: nginxPath + '/socket.io',
-        query: 'token=' + socketToken(),
-        forceNew: true
-    });
-    return {
-        on: function (eventName, callback) {
-            socket.on(eventName, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    callback.apply(socket, args);
-                });
-            });
-        },
-        emit: function (eventName, data, callback) {
-            socket.emit(eventName, data, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            })
-        }
-    };
-
-
+angular.module('csrService', []).factory('socket', ['$rootScope', '$http', '$timeout', '$q', function ($rootScope, _$http, _$timeout, _$q) {
+  const socket = io.connect(`https://${window.location.host}`, {
+    path: `${nginxPath}/socket.io`,
+    query: `token=${socketToken()}`,
+    forceNew: true
+  });
+  return {
+    on(eventName, callback) {
+      socket.on(eventName, function () {
+        const args = arguments;
+        $rootScope.$apply(() => {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit(eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        const args = arguments;
+        $rootScope.$apply(() => {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
 }]);
 
 function socketToken() {
-    var token = '';
-    $.ajax({
-        url: './token',
-        type: 'GET',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            if (data.message === "success") {
-                token = data.token;
-            }
-        }
-    });
-    return token;
+  let token = '';
+  $.ajax({
+    url: './token',
+    type: 'GET',
+    dataType: 'json',
+    async: false,
+    success(data) {
+      if (data.message === 'success') {
+        token = data.token;
+      }
+    }
+  });
+  return token;
 }
