5f29b3ec82c2823fcbda95497b6fe7403fe9470d
Ed Torres <mitrefccace@users.noreply.github.com>
2021-03-04 12:20:52 -0500

    lint app, build, cleanup js

--- userver/cleanup.js
+++ userver/cleanup.js
@@ -1,30 +1,29 @@
-function noOp() {};
-exports.Cleanup = function Cleanup(callback) {
-  callback = callback || noOp;
-  process.on('cleanup',callback);
-  process.on('exit', function () {
+function noOp() {}
+exports.Cleanup = function Cleanup(callbackParam) {
+  const callback = callbackParam || noOp;
+  process.on('cleanup', callback);
+  process.on('exit', () => {
     process.emit('cleanup');
   });
-  process.on('SIGINT', function () {
+  process.on('SIGINT', () => {
     console.log('Ctrl-C...');
     process.exit(2);
   });
-  process.on('SIGUSR1', function () {
+  process.on('SIGUSR1', () => {
     console.log('Kill 1 caught...');
     process.exit(3);
   });
-  process.on('SIGUSR2', function () {
+  process.on('SIGUSR2', () => {
     console.log('Kill 2 caught...');
     process.exit(4);
   });
-  process.on('SIGTERM', function () {
+  process.on('SIGTERM', () => {
     console.log('SIGTERM...');
     process.exit(5);
   });
-  process.on('uncaughtException', function(e) {
+  process.on('uncaughtException', (e) => {
     console.log('Uncaught Exception...');
     console.log(e.stack);
     process.exit(99);
   });
 };
-
