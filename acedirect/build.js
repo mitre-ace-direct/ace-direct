33f3d3c4a245078abefdd8006c89ab6b0f21bb03
Ed Torres <mitrefccace@users.noreply.github.com>
2021-03-10 12:58:02 -0500

    linted build.js

--- acedirect/build.js
+++ acedirect/build.js
@@ -1,61 +1,57 @@
 const path = require('path');
-const now = new Date()
-const secondsSinceEpoch = Math.round(now.getTime() / 1000)
-
-var fs = require('fs');
+const fs = require('fs');
+const execLib = require('child_process');
 
 const CSS = [
-    'bootstrap/dist/css/bootstrap.min.css',
-    'bootstrap/dist/css/bootstrap.min.css.map',
-    'bootstrap-toggle/css/bootstrap-toggle.css',
-    'font-awesome/css/font-awesome.min.css',
-    'inputmask/css/inputmask.css',
-    'admin-lte/dist/css/AdminLTE.min.css',
-    'admin-lte/dist/css/skins/skin-blue.min.css',
-    'admin-lte/dist/css/skins/skin-purple.min.css',
-    'ionicons/dist/css/ionicons.min.css',
-    'ionicons/dist/css/ionicons.min.css.map',
-    'gridstack/dist/gridstack.min.css',
-    'datatables.net-bs/css/dataTables.bootstrap.min.css',
+  'bootstrap/dist/css/bootstrap.min.css',
+  'bootstrap/dist/css/bootstrap.min.css.map',
+  'bootstrap-toggle/css/bootstrap-toggle.css',
+  'font-awesome/css/font-awesome.min.css',
+  'inputmask/css/inputmask.css',
+  'admin-lte/dist/css/AdminLTE.min.css',
+  'admin-lte/dist/css/skins/skin-blue.min.css',
+  'admin-lte/dist/css/skins/skin-purple.min.css',
+  'ionicons/dist/css/ionicons.min.css',
+  'ionicons/dist/css/ionicons.min.css.map',
+  'gridstack/dist/gridstack.min.css',
+  'datatables.net-bs/css/dataTables.bootstrap.min.css'
 
-
-    /*
+  /*
     'admin-lte/plugins/bootstrap-slider/slider.css',
     'bootstrap-daterangepicker/daterangepicker.css',
     */
 ];
 const FONT = [
-    'font-awesome/fonts/fontawesome-webfont.woff2',
-    'bootstrap/fonts/glyphicons-halflings-regular.woff',
-    'bootstrap/fonts/glyphicons-halflings-regular.woff2',
-    'bootstrap/fonts/glyphicons-halflings-regular.ttf'
+  'font-awesome/fonts/fontawesome-webfont.woff2',
+  'bootstrap/fonts/glyphicons-halflings-regular.woff',
+  'bootstrap/fonts/glyphicons-halflings-regular.woff2',
+  'bootstrap/fonts/glyphicons-halflings-regular.ttf'
 ];
 
 const JS = [
-    'bootstrap/dist/js/bootstrap.js',
-    'bootstrap-toggle/js/bootstrap-toggle.js',
-    'jquery/dist/jquery.min.js',
-    'inputmask/dist/min/inputmask/inputmask.min.js',
-    'inputmask/dist/min/inputmask/jquery.inputmask.min.js',
-    'jwt-decode/build/jwt-decode.js',
-    'jwt-decode/build/jwt-decode.js.map',
-    'moment/moment.js',
-    'admin-lte/dist/js/adminlte.min.js',
-    'admin-lte/plugins/jQueryUI/jquery-ui.min.js',
-    'gridstack/dist/gridstack.min.js',
-    'gridstack/dist/gridstack.min.map',
-    'gridstack/dist/gridstack.jQueryUI.min.js',
-    'lodash/lodash.min.js',
-    'datatables.net/js/jquery.dataTables.min.js',
-    'datatables.net-bs/js/dataTables.bootstrap.min.js',
-    /*
+  'bootstrap/dist/js/bootstrap.js',
+  'bootstrap-toggle/js/bootstrap-toggle.js',
+  'jquery/dist/jquery.min.js',
+  'inputmask/dist/min/inputmask/inputmask.min.js',
+  'inputmask/dist/min/inputmask/jquery.inputmask.min.js',
+  'jwt-decode/build/jwt-decode.js',
+  'jwt-decode/build/jwt-decode.js.map',
+  'moment/moment.js',
+  'admin-lte/dist/js/adminlte.min.js',
+  'admin-lte/plugins/jQueryUI/jquery-ui.min.js',
+  'gridstack/dist/gridstack.min.js',
+  'gridstack/dist/gridstack.min.map',
+  'gridstack/dist/gridstack.jQueryUI.min.js',
+  'lodash/lodash.min.js',
+  'datatables.net/js/jquery.dataTables.min.js',
+  'datatables.net-bs/js/dataTables.bootstrap.min.js'
+  /*
     'admin-lte/plugins/bootstrap-slider/bootstrap-slider.js',
     'bootstrap-daterangepicker/daterangepicker.js',
     'jquery-form-validator/form-validator/jquery.form-validator.min.js',
     'jquery-form-validator/form-validator/toggleDisabled.js',
     */
 ];
-
 
 function buildAssets() {
   if (!fs.existsSync('./public/assets')) {
@@ -70,66 +66,63 @@ function buildAssets() {
   if (!fs.existsSync('./public/assets/fonts')) {
     fs.mkdirSync('./public/assets/fonts');
   }
-  JS.map(asset => {
-    let filename = asset.substring(asset.lastIndexOf("/") + 1);
-    let from = path.resolve(__dirname, `./node_modules/${asset}`)
-    let to = path.resolve(__dirname, `./public/assets/js/${filename}`)
+  JS.map((asset) => {
+    const filename = asset.substring(asset.lastIndexOf('/') + 1);
+    const from = path.resolve(__dirname, `./node_modules/${asset}`);
+    const to = path.resolve(__dirname, `./public/assets/js/${filename}`);
     if (fs.existsSync(from)) {
-      fs.createReadStream(from).pipe(fs.createWriteStream(to));
-    } else {
-      console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`)
-      process.exit(1)
+      return fs.createReadStream(from).pipe(fs.createWriteStream(to));
     }
+    console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+    return process.exit(1);
   });
 
-  CSS.map(asset => {
-    let filename = asset.substring(asset.lastIndexOf("/") + 1);
-    let from = path.resolve(__dirname, `./node_modules/${asset}`)
-    let to = path.resolve(__dirname, `./public/assets/css/${filename}`)
+  CSS.map((asset) => {
+    const filename = asset.substring(asset.lastIndexOf('/') + 1);
+    const from = path.resolve(__dirname, `./node_modules/${asset}`);
+    const to = path.resolve(__dirname, `./public/assets/css/${filename}`);
     if (fs.existsSync(from)) {
-      fs.createReadStream(from).pipe(fs.createWriteStream(to));
-    } else {
-      console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`)
-      process.exit(1)
+      return fs.createReadStream(from).pipe(fs.createWriteStream(to));
     }
+    console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+    return process.exit(1);
   });
 
-  FONT.map(asset => {
-    let filename = asset.substring(asset.lastIndexOf("/") + 1);
-    let from = path.resolve(__dirname, `./node_modules/${asset}`)
-    let to = path.resolve(__dirname, `./public/assets/fonts/${filename}`)
+  FONT.map((asset) => {
+    const filename = asset.substring(asset.lastIndexOf('/') + 1);
+    const from = path.resolve(__dirname, `./node_modules/${asset}`);
+    const to = path.resolve(__dirname, `./public/assets/fonts/${filename}`);
     if (fs.existsSync(from)) {
-      fs.createReadStream(from).pipe(fs.createWriteStream(to));
-    } else {
-      console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`)
-      process.exit(1)
+      return fs.createReadStream(from).pipe(fs.createWriteStream(to));
     }
+    console.log(`${from} does not exist.\nUpdate the build.js script with the correct file paths.`);
+    return process.exit(1);
   });
 }
 
-//function to execute shell command as a promise
-//cmd is the shell command
-//wdir is the working dir
-//return a Promise
-function execCommand(cmd,wdir,expected,hint) {
-  console.log('executing  ' + cmd + '  ...');
-  const exec = require('child_process').exec;
-  return new Promise((resolve, reject) => {
-    exec(cmd, {cwd: wdir}, (error, stdout, stderr) => {
+// function to execute shell command as a promise
+// cmd is the shell command
+// wdir is the working dir
+// return a Promise
+function execCommand(cmd, wdir, expected, _hint) {
+  console.log(`executing  ${cmd}  ...`);
+  const { exec } = execLib;
+  return new Promise((resolve, _reject) => {
+    exec(cmd, { cwd: wdir }, (error, stdout, stderr) => {
       if (error) {
         console.log();
-        console.error('FAILED! Please resolve: ' + error.cmd);
+        console.error(`FAILED! Please resolve: ${error.cmd}`);
         console.log();
         process.exit(99);
       } else if (expected) {
-        expected = expected.trim();
-        stdout = stdout.trim();
-        if (stdout !== expected) {
-          console.log('FAILED! Incorrect version: ' + stdout + '. expected: ' + expected);
+        const expectedVal = expected.trim();
+        const stdoutVal = stdout.trim();
+        if (stdoutVal !== expectedVal) {
+          console.log(`FAILED! Incorrect version: ${stdout}. expected: ${expected}`);
           process.exit(99);
         }
       }
-      resolve(stdout? stdout : stderr);
+      resolve(stdout || stderr);
     });
   });
 }
@@ -137,16 +130,16 @@ function execCommand(cmd,wdir,expected,hint) {
 async function go() {
   console.log('Building acedirect...');
   console.log('checking for dat/ configuration files...');
-  s = await execCommand('ls config.json','../dat',null,'ERROR: ../dat/config.json is missing!');
-  s = await execCommand('ls default_color_config.json','../dat',null,'ERROR: ../dat/default_color_config.json is missing!');
-  s = await execCommand('ls color_config.json','../dat',null,'ERROR: ../dat/color_config.json is missing!');
+  await execCommand('ls config.json', '../dat', null, 'ERROR: ../dat/config.json is missing!');
+  await execCommand('ls default_color_config.json', '../dat', null, 'ERROR: ../dat/default_color_config.json is missing!');
+  await execCommand('ls color_config.json', '../dat', null, 'ERROR: ../dat/color_config.json is missing!');
 
-  s = await execCommand('rm -rf node_modules >/dev/null 2>&1 || true ','.',null,null);
-  s = await execCommand('npm install ','.',null,null);
-  s = await execCommand('rm -f public/assets/css/* public/assets/fonts/* public/assets/js/* public/assets/webfonts/* > /dev/null 2>&1 || true ','.',null,null);
+  await execCommand('rm -rf node_modules >/dev/null 2>&1 || true ', '.', null, null);
+  await execCommand('npm install ', '.', null, null);
+  await execCommand('rm -f public/assets/css/* public/assets/fonts/* public/assets/js/* public/assets/webfonts/* > /dev/null 2>&1 || true ', '.', null, null);
 
   buildAssets();
   console.log('SUCCESS!');
 }
 
-go(); //MAIN
+go(); // MAIN
