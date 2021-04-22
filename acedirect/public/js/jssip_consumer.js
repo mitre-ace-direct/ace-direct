638e3fd45d79efc691a1ed5cbf341d8696f5e94d
Jackie Greening <jgreening@mitre.org>
2021-04-22 15:38:00 +0000

    Fix hangups while monitoring

--- acedirect/public/js/jssip_consumer.js
+++ acedirect/public/js/jssip_consumer.js
@@ -20,6 +20,7 @@
 	var maxRecordingSeconds = 90;
 	var call_terminated = false;
 	var privacy_video_url = window.location.origin + "/" + nginxPath + "/media/videoPrivacy.webm";
+	var monitorExt;
 
 	//VIDEOMAIL recording progress bar
 	var recordId = null;
@@ -181,6 +182,7 @@
 				$("#start-call-buttons").show();
 				$("#agent-name-box").hide();
 				$("#agent-name").text("");
+				$('#end-call').attr('onclick', 'terminate_call()');
 
 			},
 			'participantsUpdate': function(e) {
@@ -190,6 +192,12 @@
 				var partCount = e.participants.filter(t=>t.type == "participant:webrtc").length;
 
 				console.log("--- WV: partCount: " + partCount);
+
+				for (var i = 0; i < e.participants.length; i++) {
+					if (e.participants[i].isMonitor) {
+						monitorExt = e.participants[i].ext;
+					}
+				}
 
 				if (partCount >=2 || videomailflag) {
 					console.log("--- WV: CONNECTED");
@@ -263,6 +271,14 @@
 		}
 	}
 
+	function monitorHangup() {
+		socket.emit('force-monitor-leave', {'monitorExt': monitorExt, 'reinvite': false});
+		
+		setTimeout(() => {
+			terminate_call();
+		}, 500);
+	}
+
 	//handles cleanup from jssip call. removes the session if it is active and removes video.
 	function terminate_call() {
 		if (acekurento !== null) {
@@ -270,6 +286,7 @@
 			acekurento = null;
 		}
 		call_terminated = true;
+		monitorExt = null;
 
 		
 		document.getElementById("screenshareButton").disabled = true;
