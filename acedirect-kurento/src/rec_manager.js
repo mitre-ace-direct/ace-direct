abe309f528ccbf75ba0769e9c14f0b9fae559aad
Eric Kosinski <ekosinski@mitre.org>
2021-05-27 13:48:32 +0000

    Includes recording length, filtering, and fixed record status change

--- acedirect-kurento/src/rec_manager.js
+++ acedirect-kurento/src/rec_manager.js
@@ -6,6 +6,9 @@ const S3Client = require('./s3_client');
 var proxy = require('proxy-agent');
 var fs = require('fs');
 const request = require('request');
+const { getVideoDurationInSeconds } = require('get-video-duration');
+var mysql = require('mysql2');
+
 /**
  * Custom logic for screen recording
  */
@@ -19,12 +22,26 @@ AWS.config.update({
 });
 var s3 = new AWS.S3();
 
+/**
+ * Custom logic for mysql connection
+ */
+
+var dbHost = param('database_servers.mysql.host');
+var dbUser = param('database_servers.mysql.user');
+var dbPassword = param('database_servers.mysql.password');
+var dbName = param('database_servers.mysql.ad_database_name');
+var dbPort = parseInt(param('database_servers.mysql.port'));
+console.log("Using host: " + dbHost);
+console.log("Using username: " + dbUser);
+console.log("Using name: " + dbName);
+console.log("Using port: " + dbPort);
+
 //AWS.config.update({region : 'us-east-1'});
 //s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 class RecordingManager extends Events {
 
-  static async createRecording(filename, peer, session_id) {
+  static async createRecording(filename, peer, session_id, agentNumber, otherCallers) {
     const id = await models.PeerRecording.create({
       filename,
       peer,
@@ -41,6 +58,39 @@ class RecordingManager extends Events {
     record.on('finish', function () {
       fs.readFile(filepath, function(err, fileData){
         uploadParams.Body = fileData;
+        getVideoDurationInSeconds(filepath).then((duration) => {
+
+          const con = mysql.createConnection(
+            {
+              host: dbHost,
+              user: dbUser,
+              password: dbPassword,
+              database: dbName,
+              port: dbPort}
+          );
+          let sqlQuery = 'INSERT INTO call_recordings (fileName, agentNumber, participants, timestamp, status, duration, deleted) VALUES (?,?,?,NOW(),?,?,?);';
+          let params = [filename, agentNumber, otherCallers, 'UNREAD', Math.floor(duration), 0];
+          console.log("QUERY is " + sqlQuery);
+          con.promise().query(sqlQuery, params)
+            .catch(console.log)
+            .then( () => con.end());
+
+          /*const dbConnection = await mysql.createConnection({
+            host: dbHost,
+            user: dbUser,
+            password: dbPassword,
+            database: dbName,
+            port: dbPort
+          });
+          let sqlQuery = 'INSERT INTO call_recordings (fileName, agentNumber, participants, timestamp, status, duration, deleted) VALUES (?,?,?,NOW(),?);';
+          let params = [filename, agentNumber, otherCallers, 'UNREAD', Math.floor(duration), 0];
+          console.log("QUERY is " + sqlQuery);
+
+          //var [rows,fields] = await dbConnection.execute(sqlQuery, params);
+          var promise = dbConnection.execute(sqlQuery, params);
+
+          await dbConnection.end();*/
+        })
         // call S3 to retrieve upload file to specified bucket
         s3.upload (uploadParams, function (err, data) {
           if (err) {
