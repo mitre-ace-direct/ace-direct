c598c08c2bd191fbd54af1ed212974cd0d74dfe7
Ed Torres <ertorres@mitre.org>
2021-06-29 20:45:47 +0000

    update status script

--- scripts/status.sh
+++ scripts/status.sh
@@ -2,7 +2,7 @@
 
 echo "Checking servers..."
 
-STATUS="\n*******************************************\nSERVER STATUS:\n"
+STATUS="\n***************************************************\n\n  SERVER STATUS:\n\n"
 
 STUN_FQDN=`grep "stun_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'`
 TURN_FQDN=`grep "turn_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'`
@@ -11,12 +11,25 @@ ASTERISK_FQDN=`grep "asterisk_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed
 MAIN_FQDN=`grep "main_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'`
 REDIS_FQDN=`grep "redis_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'`
 NGINX_FQDN=`grep "nginx_fqdn" dat/config.json|awk -F':' '{ print $2 }'|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'`
+RESET_COLORS='\u001b[0m'
+FG_RED='\u001b[31m'
+
+
+for ((i = 0; i < 7; ++i)); do
+  PM2_STATUS=`pm2 show ${i} | grep status | awk '{ print $4 }' | sed 's/ //g'`
+  FG=''
+  if [[ "$PM2_STATUS" != "online" ]]
+  then
+    FG=${FG_RED}
+  fi
+  STATUS="${STATUS}  pm2 status ${i}: ${FG}${PM2_STATUS}${RESET_COLORS}\n" 
+done
 
 
 # Check MAIN
 DISK_USAGE=`ssh "${USER}@${MAIN_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${MAIN_FQDN}  disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${MAIN_FQDN}  disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 else
   STATUS="${STATUS}  ${MAIN_FQDN}  disk usage is ${DISK_USAGE}%%.\n"
 fi
@@ -26,11 +39,11 @@ if ssh "${USER}@${STUN_FQDN}" sudo netstat -tnlp | grep 3478
 then
   STATUS="${STATUS}  ${STUN_FQDN} is UP.\n"
 else
-  STATUS="${STATUS}  ${STUN_FQDN} is DOWN.\n"
+  STATUS="${STATUS}  ${STUN_FQDN} is ${FG_RED}DOWN${RESET_COLORS}.\n"
 fi
 DISK_USAGE=`ssh "${USER}@${STUN_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${STUN_FQDN}  disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${STUN_FQDN}  disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 # Check TURN 
@@ -38,11 +51,11 @@ if ssh "${USER}@${TURN_FQDN}" sudo netstat -tnlp | grep 3478
 then
   STATUS="${STATUS}  ${TURN_FQDN} is UP.\n"
 else
-  STATUS="${STATUS}  ${TURN_FQDN} is DOWN.\n"
+  STATUS="${STATUS}  ${TURN_FQDN} is ${FG_RED}DOWN${RESET_COLORS}.\n"
 fi
 DISK_USAGE=`ssh "${USER}@${TURN_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${TURN_FQDN}  disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${TURN_FQDN}  disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 
@@ -51,11 +64,11 @@ if ssh "${USER}@${ASTERISK_FQDN}" sudo systemctl status asterisk
 then
   STATUS="${STATUS}  ${ASTERISK_FQDN} is UP.\n"
 else
-  STATUS="${STATUS}  ${ASTERISK_FQDN} is DOWN.\n"
+  STATUS="${STATUS}  ${ASTERISK_FQDN} is ${FG_RED}DOWN${RESET_COLORS}\n"
 fi
 DISK_USAGE=`ssh "${USER}@${ASTERISK_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${ASTERISK_FQDN} disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${ASTERISK_FQDN} disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 # Check REDIS
@@ -63,11 +76,11 @@ if ssh "${USER}@${REDIS_FQDN}" sudo systemctl status redis
 then
   STATUS="${STATUS}  REDIS is UP.\n"
 else
-  STATUS="${STATUS}  REDIS is DOWN.\n"
+  STATUS="${STATUS}  REDIS is ${FG_RED}DOWN${RESET_COLORS}\n"
 fi
 DISK_USAGE=`ssh "${USER}@${REDIS_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  REDIS disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  REDIS disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 # Check NGINX
@@ -75,11 +88,11 @@ if ssh "${USER}@${NGINX_FQDN}" sudo systemctl status nginx
 then
   STATUS="${STATUS}  NGINX is UP.\n"
 else
-  STATUS="${STATUS}  NGINX is DOWN.\n"
+  STATUS="${STATUS}  NGINX is ${FG_RED}DOWN${RESET_COLORS}\n"
 fi
 DISK_USAGE=`ssh "${USER}@${NGINX_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${NGINX_FQDN} disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${NGINX_FQDN} disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 # Check KMS
@@ -88,15 +101,15 @@ if ssh "${USER}@${KURENTO_FQDN}" sudo systemctl status kurento-media-server
 then
   STATUS="${STATUS}  ${KURENTO_FQDN} is UP.\n"
 else
-  STATUS="${STATUS}  ${KURENTO_FQDN} is DOWN.\n"
+  STATUS="${STATUS}  ${KURENTO_FQDN} is ${FG_RED}DOWN${RESET_COLORS}\n"
 fi
 DISK_USAGE=`ssh "${USER}@${KURENTO_FQDN}" df -k --output='pcent' / | tail -1 | sed -e 's/%//g'`
 if (( $DISK_USAGE > 95 )); then
-  STATUS="${STATUS}  ${KURENTO_FQDN} disk usage is ${DISK_USAGE}%% !!     WARNING!!!\n"
+  STATUS="${STATUS}  ${KURENTO_FQDN} disk usage is ${FG_RED}${DISK_USAGE}%%  WARNING!${RESET_COLORS}\n"
 fi
 
 
-STATUS="${STATUS}*******************************************\n\n"
+STATUS="${STATUS}\n***************************************************\n\n"
 
 clear
 echo ""
