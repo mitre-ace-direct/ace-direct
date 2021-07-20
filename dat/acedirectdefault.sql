bec749c824600ed27ff0c355b5a1dcd310ecd976
Edwin Torres <CoachEd@gmail.com>
2021-07-20 17:33:17 -0400

    overall acenode installation

--- dat/acedirectdefault.sql
+++ dat/acedirectdefault.sql
@@ -359,8 +359,8 @@ UNLOCK TABLES;
 -- Create acedirect and asterisk users and set appropriate permissions.
 -- For security, the acedirect user only gets read access to the CDR table.
 
-DROP USER 'acedirect';
-DROP USER 'asterisk';
+DROP USER IF EXISTS 'acedirect';
+DROP USER IF EXISTS 'asterisk';
 CREATE USER 'acedirect'@'%' IDENTIFIED BY '_ACEDIRECT_PASSWORD_';
 CREATE USER 'asterisk'@'%' IDENTIFIED BY '_ASTERISK_PASSWORD_';
 
