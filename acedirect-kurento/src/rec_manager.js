e93605edf8dd8611df7ac58cdef25d380e1d9f62
Jackie Greening <jgreening@mitre.org>
2021-08-16 18:11:20 +0000

    First lint passthrough for rec_manager.js

--- acedirect-kurento/src/rec_manager.js
+++ acedirect-kurento/src/rec_manager.js
@@ -1,7 +1,7 @@
-const debug    = require('debug')('ace:rec-man');
-const param    = require('param');
-const Events   = require('events');
-const models   = require('./dal/models');
+const debug = require('debug')('ace:rec-man');
+const param = require('param');
+const Events = require('events');
+const models = require('./dal/models');
 const S3Client = require('./s3_client');
 var proxy = require('proxy-agent');
 var fs = require('fs');
@@ -37,23 +37,22 @@ console.log("Using name: " + dbName);
 console.log("Using port: " + dbPort);
 
 class RecordingManager extends Events {
-
   static async createRecording(filename, peer, session_id, agentNumber, otherCallers) {
     const id = await models.PeerRecording.create({
       filename,
       peer,
       session_id
     });
-    //debug('Created recording for %s: %s', peer, id);
-    //Custom logic to try uploading the recording to the s3 bucket
-    var uploadParams = {Bucket: param('s3.bucketname'), Key: filename, Body: ""};
+    // debug('Created recording for %s: %s', peer, id);
+    // Custom logic to try uploading the recording to the s3 bucket
+    var uploadParams = { Bucket: param('s3.bucketname'), Key: filename, Body: "" };
 
     let fileRequest = "http://" + param('servers.kurento_private_ip') + ":" + param('app_ports.kmsshare') + "/recordings/" + filename;
     const filepath = 'media/' + filename;
     let record = fs.createWriteStream(filepath);
     request(fileRequest).pipe(record);
     record.on('finish', function () {
-      fs.readFile(filepath, function(err, fileData){
+      fs.readFile(filepath, function (err, fileData) {
         uploadParams.Body = fileData;
         getVideoDurationInSeconds(filepath).then((duration) => {
           const con = mysql.createConnection(
@@ -62,40 +61,42 @@ class RecordingManager extends Events {
               user: dbUser,
               password: dbPassword,
               database: dbName,
-              port: dbPort}
+              port: dbPort
+            }
           );
           let sqlQuery = 'INSERT INTO call_recordings (fileName, agentNumber, participants, timestamp, status, duration, deleted) VALUES (?,?,?,NOW(),?,?,?);';
           let params = [filename, agentNumber, otherCallers, 'UNREAD', Math.floor(duration), 0];
           con.promise().query(sqlQuery, params)
             .catch(console.log)
-            .then( () => con.end());
-
-        })
+            .then(() => con.end());
+        });
         // call S3 to retrieve upload file to specified bucket
-        s3.upload (uploadParams, function (err, data) {
+        s3.upload(uploadParams, function (err, data) {
           if (err) {
             console.log("Error", err);
           } if (data) {
             console.log("Upload Success", data.Location);
-            //Delete file from media folder
+            // Delete file from media folder
           }
         });
       });
     });
-}
+  }
 
-  static async listRecordings({ peer = null, session_id = null, date_interval = null, start = 0 }) {
+  static async listRecordings({
+    peer = null, session_id = null, date_interval = null, start = 0
+  }) {
     const where = {};
-    if(peer) where.peer = peer;
-    if(session_id) where.session_id = session_id;
-    if(date_interval) {
+    if (peer) where.peer = peer;
+    if (session_id) where.session_id = session_id;
+    if (date_interval) {
       const { from, to } = date_interval;
-      if(from && to) where.created_at = { $between: [from, to]}
-      else if(from) where.created_at = { $gt: from };
-      else if(to) where.created_at = { $lt: to };
+      if (from && to) where.created_at = { $between: [from, to] };
+      else if (from) where.created_at = { $gt: from };
+      else if (to) where.created_at = { $lt: to };
     }
     const recordings = await models.PeerRecording.findAll({
-      where 
+      where
     });
 
     const s3 = new S3Client();
