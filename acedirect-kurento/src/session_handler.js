f341160aab048ea6dd2044bf2f2dbb0c0a8580cd
Jackie Greening <jgreening@mitre.org>
2021-08-16 19:02:47 +0000

    First lint passthrough for session_handler.js and session_index.js

--- acedirect-kurento/src/session_handler.js
+++ acedirect-kurento/src/session_handler.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const debug   = require('debug')('ace:session-handler');
-const param   = require('param');
-const models  = require('./dal/models');
+const debug = require('debug')('ace:session-handler');
+const param = require('param');
+const models = require('./dal/models');
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
       res.json(sessions);
     } catch (ex) {
       debug('Error on /session', ex.message);
-      res.status(500).json({error: 'An error occurred'});
+      res.status(500).json({ error: 'An error occurred' });
     }
   } else {
     res.status(404).send('Not found. Monitoring Disabled');
@@ -35,7 +35,7 @@ router.get('/:id/stats', async (req, res) => {
       res.json(stats);
     } catch (ex) {
       debug('Error on /session/%s/stats', req.params.id, ex.message);
-      res.status(500).json({error: 'An error occurred'});
+      res.status(500).json({ error: 'An error occurred' });
     }
   } else {
     res.status(404).send('Not found. Monitoring Disabled');
