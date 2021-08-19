3dd4b416b3cfba8460d0568736c5504093587e41
Jackie Greening <jgreening@mitre.org>
2021-08-19 20:13:05 +0000

    Second lint pass

--- acedirect-kurento/src/redis_client.js
+++ acedirect-kurento/src/redis_client.js
@@ -3,10 +3,10 @@ const param = require('param');
 
 const client = redis.createClient(param('database_servers.redis.host'), param('database_servers.redis.host'));
 client.auth(param('database_servers.redis.auth'));
-client.on('error', function (err) {
-  console.log('could not establish a connection with redis. ' + err);
+client.on('error', (err) => {
+  console.log(`could not establish a connection with redis. ${err}`);
 });
-client.on('connect', function (err) {
+client.on('connect', (_err) => {
   console.log('connected to redis successfully');
 });
 
