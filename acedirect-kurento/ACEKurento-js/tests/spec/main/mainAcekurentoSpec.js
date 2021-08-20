a29b669f270a6491935b304e7ab9f05b2490bf52
Jackie Greening <jgreening@mitre.org>
2021-08-20 13:23:22 +0000

    Second lint pass

--- acedirect-kurento/ACEKurento-js/tests/spec/main/mainAcekurentoSpec.js
+++ acedirect-kurento/ACEKurento-js/tests/spec/main/mainAcekurentoSpec.js
@@ -1,17 +1,17 @@
-describe("ACEKurento", function () {
-  var acekurento;
+describe('ACEKurento', () => {
+  let acekurento;
 
-  beforeEach(function () {
+  beforeEach(() => {
     acekurento = new ACEKurento();
     spyOn(console, 'log');
   });
 
-  it("should be defined", function () {
+  it('should be defined', () => {
     expect(acekurento).toBeDefined();
   });
 
-  it("should be able to instantiate a second object different than the previous one", function () {
-    var acekurento2 = new ACEKurento();
+  it('should be able to instantiate a second object different than the previous one', () => {
+    const acekurento2 = new ACEKurento();
     expect(acekurento).not.toEqual(acekurento2);
   });
 });
