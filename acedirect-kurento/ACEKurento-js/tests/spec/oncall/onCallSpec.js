6d6e4e23543408033eaf7761c46c0767dc4e6543
Jackie Greening <jgreening@mitre.org>
2021-08-20 13:23:22 +0000

    Second lint pass

--- acedirect-kurento/ACEKurento-js/tests/spec/oncall/onCallSpec.js
+++ acedirect-kurento/ACEKurento-js/tests/spec/oncall/onCallSpec.js
@@ -1,48 +1,48 @@
-describe("ACEKurento oncall outbound", function () {
-  var acekurento;
-  var ext = '1000';
-  var WSSpy;
+describe('ACEKurento oncall outbound', () => {
+  let acekurento;
+  const ext = '1000';
+  let WSSpy;
   const NO_CALL = 0;
   const PROCESSING_CALL = 1;
   const IN_CALL = 2;
   // mock credentials
-  var address = 'wss://localhost:8443/signaling';
-  var uri = 'sip:1000@acekurento-test.com:5060';
-  var username = 'username';
-  var password = 'af8KSZQREMwov';
-  var sessionStoppedMessage = {
-    "data": {
-      "id": "sessionStopped",
-      "session": "1234"
+  const address = 'wss://localhost:8443/signaling';
+  const uri = 'sip:1000@acekurento-test.com:5060';
+  const username = 'username';
+  const password = 'af8KSZQREMwov';
+  const sessionStoppedMessage = {
+    data: {
+      id: 'sessionStopped',
+      session: '1234'
     }
   };
 
-  beforeEach(function (done) {
-    var realWS = WebSocket;
-    WSSpy = spyOn(window, "WebSocket").and.callFake(function (url, protocols) {
+  beforeEach((done) => {
+    const realWS = WebSocket;
+    WSSpy = spyOn(window, 'WebSocket').and.callFake((url, protocols) => {
       return new realWS(url, protocols);
     });
     acekurento = new ACEKurento({ acekurentoSignalingUrl: address });
     spyOn(console, 'log');
-    setTimeout(function () {
+    setTimeout(() => {
       console.info('rendering time...');
       acekurento.call(uri, true);
       done();
     }, 1000);
   });
 
-  it("should know when the call is in progress", function () {
+  it('should know when the call is in progress', () => {
     expect(acekurento.callState).toEqual(PROCESSING_CALL);
   });
 
-  it("should not be possible to call while on call", function () {
+  it('should not be possible to call while on call', () => {
     acekurento.call(uri, true);
-    expect(console.log).toHaveBeenCalledWith("You are already on a call");
+    expect(console.log).toHaveBeenCalledWith('You are already on a call');
   });
 
-  describe("stop", function () {
-    it("should set call state to ended", function (done) {
-      setTimeout(function () {
+  describe('stop', () => {
+    it('should set call state to ended', (done) => {
+      setTimeout(() => {
         acekurento.stop(true);
         console.log(acekurento.callState);
         expect(acekurento.callState).toEqual(NO_CALL);
