e9e5f627bcb1b41f7b398c8314f49c5c4ccf6651
Jackie Greening <jgreening@mitre.org>
2021-08-24 13:29:58 +0000

    Second lint pass

--- acedirect-kurento/src/dal/migrations/20181120011610-create-webrtc-session-table.js
+++ acedirect-kurento/src/dal/migrations/20181120011610-create-webrtc-session-table.js
@@ -1,34 +1,30 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('ace_direct_webrtc_session', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      session_id: {
-        type: Sequelize.UUID,
-        allowNull: false
-      },
-      from: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-      },
-      to: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
-    });
-  },
+  up: (queryInterface, Sequelize) => queryInterface.createTable('ace_direct_webrtc_session', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    session_id: {
+      type: Sequelize.UUID,
+      allowNull: false
+    },
+    from: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    to: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+  }),
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('ace_direct_webrtc_session');
-  }
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('ace_direct_webrtc_session')
 };
