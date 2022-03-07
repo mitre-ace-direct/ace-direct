623919fc7ff4a4e8fdd1dfd9692a05b9258bb68d
Edwin Torres <CoachEd@gmail.com>
2022-03-07 13:50:12 -0500

    REMOVING IAM-OpenAM

--- iam/configure-openam2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#
-java -Djavax.net.ssl.trustStore=../keystore/iamkeystore -jar //home/centos/src/docker/iam/iam/config/oam/SSOConfiguratorTools-13.0.0/openam-configurator-tool*.jar --file /home/centos/src/docker/iam/iam/config/oam/config.properties --acceptLicense
