949a438fb7523bff68bf1aa4c452ba858f4e6639
Eric Kosinski (ekosinski@mitre.org) <ekosinski@wlad-dev-node.task3acrdemo.com>
2022-04-14 20:24:30 +0000

    Added call page videos and buttons

---
+++ acedirect/public/dro/js/complaint_form.js
@@ -0,0 +1,6 @@
+let inCall = false;
+
+function startCall(){
+    document.getElementById("noCallPoster").style.display = "none";
+    document.getElementById("inCallSection").style.display = "block";
+}
