e41545f6060d9065e85d383e66a8e674503898f2
Paul Jacyk <pjacyk@wlad-dev-node.task3acrdemo.com>
2022-05-09 20:30:46 +0000

    dro login focus trap  modal

--- acedirect/public/dro/js/complaint_login.js
+++ acedirect/public/dro/js/complaint_login.js
@@ -1,11 +1,11 @@
 function fnBrowserDetect() {
   const { userAgent } = navigator;
-  let isChromeorEdge = false;
+  let isChrome = false;
   let browserName;
   console.log(`BROWSER IS ${userAgent}`);
 
   if (userAgent.match(/chrome|chromium|crios/i)) {
-    isChromeorEdge = true;
+    isChrome = true;
   } else if (userAgent.match(/firefox|fxios/i)) {
     browserName = 'Firefox';
   } else if (userAgent.match(/safari/i)) {
@@ -13,12 +13,12 @@ function fnBrowserDetect() {
   } else if (userAgent.match(/opr\//i)) {
     browserName = 'Opera';
   } else if (userAgent.match(/edg/i)) {
-    isChromeorEdge = true;
+    browserName = 'Edge';
   } else {
     browserName = 'Unknown Browser';
   }
 
-  if (!isChromeorEdge) {
+  if (!isChrome) {
     // show modal
     $('#browserType').text(browserName);
     $('#notChromeModal').modal('show');
@@ -113,45 +113,56 @@ function login() {
   }
 }
 
-function fnBrowserDetect(){
-    let userAgent = navigator.userAgent;
-    let isChrome = false;
-    let browserName;
-    console.log("BROWSER IS " + userAgent)
-    
-    if(userAgent.match(/chrome|chromium|crios/i)) {
-        isChrome = true;
-    } else if(userAgent.match(/firefox|fxios/i)) {
-        browserName = "Firefox";
-    }  else if(userAgent.match(/safari/i)) {
-        browserName = "Safari";
-    } else if(userAgent.match(/opr\//i)) {
-        browserName = "Opera";
-    } else if(userAgent.match(/edg/i)) {
-        browserName = "Edge"
-    } else {
-        browserName="Unknown Browser";
-    }
-
-    if (!isChrome) {
-        // show modal
-        $('#browserType').text(browserName);
-        $("#notChromeModal").modal("show");
-    } 
-}
-
 function copyURL() {
-    navigator.clipboard.writeText(window.location.href).then(function() {
-        /* clipboard write success */
-        $('#clipboardStatus').text('Successfully copied support portal link to clipboard.')
-        $('#clipboardStatus').show();
-    }, function() {
-        /* clipboard write failure */
-        $('#clipboardStatus').text('Error copying support portal link to clipboard.')
-        $('#clipboardStatus').show();
-    });
+  navigator.clipboard.writeText(window.location.href).then(() => {
+    /* clipboard write success */
+    $('#clipboardStatus').text('Successfully copied support portal link to clipboard.');
+    $('#clipboardStatus').show();
+  }, () => {
+    /* clipboard write failure */
+    $('#clipboardStatus').text('Error copying support portal link to clipboard.');
+    $('#clipboardStatus').show();
+  });
 }
 
 $('#continueButton').on('click', login);
 
 $('#copy-btn').on('click', copyURL);
+
+// Every time a modal is shown, if it has an autofocus element, focus on it.
+$('#notChromeModal').on('shown.bs.modal', () => {
+  $('#continue-btn').focus();
+  console.log('shown.bs.modal');
+});
+
+// Add all the elements inside modal which you want to make focusable
+const focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+const modal = document.querySelector('#notChromeModal'); // select the modal by it's id
+
+// get first element to be focused inside modal
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0];
+const focusableContent = modal.querySelectorAll(focusableElements);
+// Get last element to be focused inside modal
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+document.addEventListener('keydown', (e) => {
+  const isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      e.preventDefault();
+    }
+  } else if (document.activeElement === lastFocusableElement) {
+    // if focused has reached to last focusable element then focus first focusable
+    // element after pressing tab
+    firstFocusableElement.focus(); // add focus for the first focusable element
+    e.preventDefault();
+  }
+});
+
+firstFocusableElement.focus();
