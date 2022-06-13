425be89f29e7b942b33bec6a1972e5b49199e7ab
Davian Albarran <dalbarran@mitre.org>
2022-06-13 15:44:48 +0000

    cleaned up some code and added a comment.

--- acedirect/adserver.js
+++ acedirect/adserver.js
@@ -3264,7 +3264,7 @@ setInterval(() => {
       startTimeUTC = data.start; // hh:mm in UTC
       endTimeUTC = data.end; // hh:mm in UTC
     }
-    sendEmit('call-center-closed', { closed: !isOpen, redirectLink: `${datConfig.servers.nginx_fqdn}/${datConfig.nginx.ad_path}/videomail` });
+    sendEmit('call-center-closed', { closed: !isOpen });
   });
 }, 5000);
 
