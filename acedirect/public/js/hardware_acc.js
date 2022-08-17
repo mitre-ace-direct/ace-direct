a3722c81d2e320aa2079cc53608b1f2548444a59
Davian Albarran <dalbarran@mitre.org>
2022-08-17 22:10:35 +0000

    i have the warning up and runnning in the agent home page. client portal is next.

--- acedirect/public/js/hardware_acc.js
+++ acedirect/public/js/hardware_acc.js
@@ -1,7 +1,19 @@
+/**
+ * This module is meant to detect if a user's browser has hardware acceleration turned on.
+ * Include it wherever this is needed.
+ */
+
 import { getGPUTier } from 'detect-gpu';
 
 $(document).ready(async () => {
+  $('hardware-acc-warning').hide();
+
   const gpu = await getGPUTier();
 
-  console.log('gpu tier result', gpu);
+  if (gpu.tier > 1) {
+    console.log('Hardware Acceleration is on.');
+    $('hardware-acc-warning').show();
+  } else {
+    console.log('Hardware Acceleration is off.');
+  }
 });
