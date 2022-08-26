1b65e38e7a97f1c8fa616dd27960e68421d4480f
Davian Albarran <dalbarran@mitre.org>
2022-08-26 16:32:32 +0000

    set up persistence between login and home consumer pages so that the banner does not load in the home page if consumer cloed it in the login page.

--- acedirect/public/dro/js/hardware_acc.js
+++ acedirect/public/dro/js/hardware_acc.js
@@ -5,14 +5,24 @@
 
 import { getGPUTier } from 'detect-gpu';
 
-$(document).ready(async () => {
+$(window).on('load', async () => {
+  const gpuBannerValue = sessionStorage.getItem('gpu-banner');
+  console.log(gpuBannerValue);
   $('#hardware-acc-warning').hide();
+
+  $('#gpu-close').click(() => {
+    console.log('close button clicked');
+    sessionStorage.setItem('gpu-banner', 'hidden');
+  });
 
   const gpu = await getGPUTier();
 
-  if (gpu.tier > 1) {
+  if (gpuBannerValue === 'hidden') {
+    $('#hardware-acc-warning').hide();
+  } else if (gpu.tier > 1 && gpuBannerValue !== 'hidden') {
     console.log('Hardware Acceleration is on.');
     $('#hardware-acc-warning').show();
+    sessionStorage.setItem('gpu-banner', 'shown');
   } else {
     console.log('Hardware Acceleration is off.');
     $('#hardware-acc-warning').hide();
