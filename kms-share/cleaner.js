a3596d65d7b1478a3aab48ae0e462eaabcc7643c
Edwin Torres <CoachEd@gmail.com>
2022-09-30 10:47:04 -0400

    linting mserver too, disable linting for videomail-service for now

--- kms-share/cleaner.js
+++ kms-share/cleaner.js
@@ -1,35 +1,34 @@
 const fs = require('fs');
+
 const path = './media/recordings/';
 
-module.exports = function() {
-    const now = new Date().getTime(); 
+module.exports = () => {
+  const now = new Date().getTime();
 
-    fs.readdir(path, (err, files) => {
-       if (err){
-          console.log(err);
-	  return;
-       }
-       files.forEach(file => {
-	 if(file != 'README.md'){
-	 fs.stat(path + file, (err, stats) => {
-            if (err){
-	       console.log(err);
-	       return;
-	    }
-            let diff = now - stats.mtimeMs;
-            if(diff > 3600000){ 
-	       fs.unlink(path + file, (err) => {
-                  if (err) {
-                     console.error(err);
-	             return;
-                  }
-		  console.log(file + ' deleted');
-               });
-	    }
-         });
-	 }
-       });
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    files.forEach((file) => {
+      if (file !== 'README.md') {
+        fs.stat(path + file, (err1, stats) => {
+          if (err1) {
+            console.log(err1);
+            return;
+          }
+          const diff = now - stats.mtimeMs;
+          if (diff > 3600000) {
+            fs.unlink(path + file, (err2) => {
+              if (err2) {
+                console.error(err2);
+                return;
+              }
+              console.log(`${file} deleted`);
+            });
+          }
+        });
+      }
     });
-
+  });
 };
-
