cb0a0f3ae67090b3c9afb7c3bcb2368d3d7dc03a
Paul Jacyk <pjacyk@wlad-dev-node.task3acrdemo.com>
2022-10-18 19:07:16 +0000

    linting routes.js pass 2

--- acedirect/app/routes.js
+++ acedirect/app/routes.js
@@ -7,15 +7,21 @@ const shortid = require('shortid');
 const AWS = require('aws-sdk');
 const proxy = require('proxy-agent');
 const fs = require('fs');
-const utils = require('./utils.js');
-const c = require('./constants.js');
-const config = require('./../../dat/config.json');
+const multer = require('multer');
 const path = require('path');
 const formidable = require('formidable');
+const NodeClam = require('clamscan');
+const os = require('os');
+const log4js = require('log4js');
+const utils = require('./utils');
+const c = require('./constants');
+const config = require('../../dat/config.json');
+
+const logger = log4js.getLogger('ad_server');
 
 let fileSharingEnabled = false;
 if (config.filesharing && config.filesharing.enabled) {
-  fileSharingEnabled = (utils.getConfigVal(config.filesharing.enabled) === 'true') ? true : false;
+  fileSharingEnabled = (utils.getConfigVal(config.filesharing.enabled) === 'true');
 }
 
 let autoplayVideos = '';
@@ -48,7 +54,7 @@ function consumerRestrict(req, res, next) {
   if (req.session.user && req.session.user.role === 'VRS') {
     next();
   } else {
-    res.redirect('.' + utils.getConfigVal(config.nginx.consumer_route));
+    res.redirect(`.${utils.getConfigVal(config.nginx.consumer_route)}`);
   }
 }
 
@@ -60,36 +66,34 @@ function restrict(req, res, next) {
   }
 }
 
-const getAgent = (usnm) => {
-  return new Promise((resolve, reject) => {
-    console.log('Getting agent! Username: ', usnm);
-    console.log('get agent link', `https://${config.servers.main_private_ip}:${config.app_ports.mserver}/getagentrec/${usnm}`);
-    request({
-      method: 'GET',
-      headers: { 'Accept': 'application/json' },
-      url: `https://${config.servers.main_private_ip}:${config.app_ports.mserver}/getagentrec/${usnm}`,
-    }, function (error, response, data) {
-      if (error) {
-        console.log('Error! Could not get agent:', error);
-        reject(error);
-      } else {
-        console.log('Success! Agent found!');
-        console.log('Data: ', typeof data);
-        console.log('TEXT ' + JSON.parse(data));
-        if (data.length > 0) {
-          let jsonData = JSON.parse(data);
-          resolve(jsonData);
-        } else reject('Agent cannot be found!');
-      }
-    });
+const getAgent = (usnm) => new Promise((resolve, reject) => {
+  console.log('Getting agent! Username: ', usnm);
+  console.log('get agent link', `https://${config.servers.main_private_ip}:${config.app_ports.mserver}/getagentrec/${usnm}`);
+  request({
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+    url: `https://${config.servers.main_private_ip}:${config.app_ports.mserver}/getagentrec/${usnm}`
+  }, (error, _response, data) => {
+    if (error) {
+      console.log('Error! Could not get agent:', error);
+      reject(error);
+    } else {
+      console.log('Success! Agent found!');
+      console.log('Data: ', typeof data);
+      console.log(`TEXT ${JSON.parse(data)}`);
+      if (data.length > 0) {
+        const jsonData = JSON.parse(data);
+        resolve(jsonData);
+      } else reject(new Error('Agent cannot be found!'));
+    }
   });
-};
-
-router.get('/', (req, res, next) => {
-  res.redirect('.' + utils.getConfigVal(config.nginx.consumer_route));
 });
 
-router.get(utils.getConfigVal(config.nginx.consumer_route), (req, res, next) => {
+router.get('/', (req, res, _next) => {
+  res.redirect(`.${utils.getConfigVal(config.nginx.consumer_route)}`);
+});
+
+router.get(utils.getConfigVal(config.nginx.consumer_route), (req, res, _next) => {
   if (req.session.user && req.session.user.role === 'VRS') {
     let please_wait_video = 'sample.mp4'; // TODO: Update with real default video
     let instructions_video = 'sample.mp4'; // TODO: Update with real default video
@@ -106,7 +110,8 @@ router.get(utils.getConfigVal(config.nginx.consumer_route), (req, res, next) => 
       }
     }
     if (config.complaint_videos && config.complaint_videos.please_wait_video_transcript) {
-      please_wait_video_transcript = utils.getConfigVal(config.complaint_videos.please_wait_video_transcript);
+      please_wait_video_transcript = utils
+        .getConfigVal(config.complaint_videos.please_wait_video_transcript);
       if (!please_wait_video_transcript || please_wait_video_transcript.length === 0) {
         please_wait_video_transcript = config.complaint_videos.please_wait_video_transcript;
       }
@@ -119,7 +124,8 @@ router.get(utils.getConfigVal(config.nginx.consumer_route), (req, res, next) => 
       }
     }
     if (config.complaint_videos && config.complaint_videos.instructions_video_transcript) {
-      instructions_video_transcript = utils.getConfigVal(config.complaint_videos.instructions_video_transcript);
+      instructions_video_transcript = utils
+        .getConfigVal(config.complaint_videos.instructions_video_transcript);
       if (!instructions_video_transcript || instructions_video_transcript.length === 0) {
         instructions_video_transcript = config.complaint_videos.instructions_video_transcript;
       }
@@ -132,19 +138,20 @@ router.get(utils.getConfigVal(config.nginx.consumer_route), (req, res, next) => 
       }
     }
     if (config.complaint_videos && config.complaint_videos.no_agents_video_transcript) {
-      no_agents_video_transcript = utils.getConfigVal(config.complaint_videos.no_agents_video_transcript);
+      no_agents_video_transcript = utils
+        .getConfigVal(config.complaint_videos.no_agents_video_transcript);
       if (!no_agents_video_transcript || no_agents_video_transcript.length === 0) {
         no_agents_video_transcript = config.complaint_videos.no_agents_video_transcript;
       }
     }
 
     res.render('dro/pages/complaint_form', {
-      please_wait_video: please_wait_video,
-      please_wait_video_transcript: please_wait_video_transcript,
-      instructions_video: instructions_video,
-      instructions_video_transcript: instructions_video_transcript,
-      no_agents_video: no_agents_video,
-      no_agents_video_transcript: no_agents_video_transcript,
+      please_wait_video,
+      please_wait_video_transcript,
+      instructions_video,
+      instructions_video_transcript,
+      no_agents_video,
+      no_agents_video_transcript,
       screenSharingVisibility
     });
   } else {
@@ -199,7 +206,8 @@ router.get('/token', restrict, (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     const vrsnum = req.session.user.vrs;
     if (/^\d+$/.test(vrsnum)) {
-      utils.getCallerInfo(vrsnum, (vrs) => {
+      utils.getCallerInfo(vrsnum, (vrsIn) => {
+        const vrs = vrsIn;
         if (vrs.message === 'success') {
           // add isOpen flag; notifies Consumers who try to connect after hours
           vrs.data[0].isOpen = res.locals.isOpen;
@@ -207,9 +215,10 @@ router.get('/token', restrict, (req, res) => {
           vrs.data[0].startTimeUTC = res.locals.startTimeUTC; // hh:mm in UTC
           vrs.data[0].endTimeUTC = res.locals.endTimeUTC; // hh:mm in UTC
 
-          const token = jwt.sign(vrs.data[0], utils.getConfigVal(config.web_security.json_web_token.secret_key), {
-            expiresIn: '2000'
-          });
+          const token = jwt.sign(vrs.data[0],
+            utils.getConfigVal(config.web_security.json_web_token.secret_key), {
+              expiresIn: '2000'
+            });
           res.status(200).json({
             message: 'success',
             token
@@ -277,9 +286,10 @@ router.get('/token', restrict, (req, res) => {
     req.redisClient.hset(c.R_AGENT_INFO_MAP, payload.username, JSON.stringify(agentInfo));
     // sendAgentStatusList(payload.username, 'AWAY');
 
-    const token = jwt.sign(payload, utils.getConfigVal(config.web_security.json_web_token.secret_key), {
-      expiresIn: '2000'
-    });
+    const token = jwt.sign(payload,
+      utils.getConfigVal(config.web_security.json_web_token.secret_key), {
+        expiresIn: '2000'
+      });
     res.status(200).json({
       message: 'success',
       token
@@ -303,9 +313,9 @@ router.get(utils.getConfigVal(config.nginx.agent_route), agentRestrict, (req, re
   if (req.session.user.skipsetup) {
     next();
   } else {
-    const username = req.session.user.username;
+    const { username } = req.session.user;
     utils.getUserInfo(username, (user) => {
-      console.log('AGENT LOGIN ' + JSON.stringify(user));
+      console.log(`AGENT LOGIN ${JSON.stringify(user)}`);
       if (user.message === 'success') {
         req.redisClient.hget(c.R_STATUS_MAP, user.data[0].username, (_err, status) => {
           if (status !== null) {
@@ -314,8 +324,8 @@ router.get(utils.getConfigVal(config.nginx.agent_route), agentRestrict, (req, re
             });
             return;
           }
-          req.redisClient.hget(c.R_TOKEN_MAP, user.data[0].extension, (err, tokenMap) => {
-            tokenMap = JSON.parse(tokenMap);
+          req.redisClient.hget(c.R_TOKEN_MAP, user.data[0].extension, (_errHget, tokenMapIn) => {
+            let tokenMap = JSON.parse(tokenMapIn);
             const d = new Date();
             const now = d.getTime();
             // Delete Token if its older than 24 hours
@@ -357,7 +367,8 @@ router.get(utils.getConfigVal(config.nginx.agent_route), agentRestrict, (req, re
             req.session.user.stunServer = stunServer;
             req.session.user.wsPort = wsPort;
             req.session.user.signalingServerUrl = `${utils.getConfigVal(config.signaling_server.protocol)}://${utils.getConfigVal(config.servers.nginx_fqdn)}${utils.getConfigVal(config.signaling_server.path)}`;
-            req.session.user.queuesComplaintNumber = utils.getConfigVal(config.asterisk.queues.complaint.number); // why is this needed?
+            req.session.user.queuesComplaintNumber = utils
+              .getConfigVal(config.asterisk.queues.complaint.number); // why is this needed?
             req.session.user.extensionPassword = extensionPassword;
             req.session.user.complaint_queue_count = 0; // complaintQueueCount;
             req.session.user.general_queue_count = 0; // generalQueueCount;
@@ -392,7 +403,9 @@ router.get(utils.getConfigVal(config.nginx.agent_route), agentRestrict, (req, re
 
 router.get('/getVideomail', agentRestrict, (req, res) => {
   const videoId = req.query.id;
-  const agentExt = req.session.user.extension;
+  // agentExt not used anywhere? Comment it out.
+  // const agentExt = req.session.user.extension;
+
   // Wrap in mysql query
   req.dbConnection.query('SELECT video_filepath AS filepath, video_filename AS filename FROM videomail WHERE id = ?', videoId, (err, result) => {
     if (err) {
@@ -401,7 +414,9 @@ router.get('/getVideomail', agentRestrict, (req, res) => {
       console.log(`|${result[0].filepath}|`);
       if (result[0].filepath === 's3') {
         console.log(result[0].filename);
-        const file = s3.getObject({ Bucket: utils.getConfigVal(config.s3.bucketname), Key: result[0].filename });
+        const file = s3.getObject(
+          { Bucket: utils.getConfigVal(config.s3.bucketname), Key: result[0].filename }
+        );
 
         res.writeHead(200, {
           'Content-Type': 'video/webm',
@@ -435,7 +450,9 @@ router.get('/getVideomail', agentRestrict, (req, res) => {
    */
 router.get('/getRecording', agentRestrict, (req, res) => {
   console.log(`USING ${req.query.fileName}`);
-  const file = s3.getObject({ Bucket: utils.getConfigVal(config.s3.bucketname), Key: req.query.fileName });
+  const file = s3.getObject(
+    { Bucket: utils.getConfigVal(config.s3.bucketname), Key: req.query.fileName }
+  );
 
   res.attachment(req.query.fileName);
   const filestream = file.createReadStream();
@@ -446,7 +463,6 @@ router.get('/getRecording', agentRestrict, (req, res) => {
 // TODO Needs middleware for agent and consumer
 // Use app,get for cooki to see if auth,  If not kicked
 // Clam AV
-const NodeClam = require('clamscan');
 let ClamScan = null;
 if (utils.getConfigVal(config.filesharing.virus_scan_enabled) === 'true' && fileSharingEnabled) {
   ClamScan = new NodeClam().init({
@@ -479,7 +495,6 @@ if (utils.getConfigVal(config.filesharing.virus_scan_enabled) === 'true' && file
   });
 }
 
-const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 router.post('/fileUpload', restrict, upload.single('uploadfile'), (req, res) => {
   let uploadedBy = req.session.user.vrs || ((req.session.user.role === 'AD Agent') ? req.body.vrs : false);
@@ -531,13 +546,15 @@ router.post('/fileUpload', restrict, upload.single('uploadfile'), (req, res) => 
     if (utils.getConfigVal(config.filesharing.virus_scan_enabled) === 'true') {
       ClamScan.then(async (clamscan) => {
         try {
-          console.log('scanning', uploadMetadata.filepath, 'as', require('os').userInfo().username, fs.existsSync(uploadMetadata.filepath));
+          console.log('scanning', uploadMetadata.filepath, 'as', os.userInfo().username, fs.existsSync(uploadMetadata.filepath));
 
           // You can re-use the `clamscan` object as many times as you want
           // const version = await clamscan.get_version();
           // console.log(`ClamAV Version: ${version}`);
 
-          const { isInfected, file, viruses } = await clamscan.is_infected(uploadMetadata.filepath);
+          // const { isInfected, file, viruses } = await clamscan
+          //   .is_infected(uploadMetadata.filepath);
+          const { isInfected, viruses } = await clamscan.is_infected(uploadMetadata.filepath);
           if (isInfected) {
             console.log(`${req.file.originalname} is infected with ${viruses}!`);
             res.status(400).send('Error scanning file i');
@@ -594,7 +611,7 @@ router.post('/fileUpload', restrict, upload.single('uploadfile'), (req, res) => 
 });
 
 router.get('/getagentstatus/:token', (req, res) => {
-  let resObj = {
+  const resObj = {
     status: 'Unknown',
     r: 0,
     g: 0,
@@ -682,8 +699,8 @@ router.get('/getagentstatus/:token', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.session.user = null;
-  req.session.save(function (err1) {
-    req.session.regenerate(function (err2) {
+  req.session.save((_err1) => {
+    req.session.regenerate((_err2) => {
       res.redirect(req.get('referer'));
     });
   });
@@ -703,38 +720,38 @@ router.get('/profilePic', (req, res) => {
     console.log('TYPEOF DATA:', typeof data);
     console.log('data.data[0].profile_picture:', data.data[0].profile_picture);
     console.log('typeof data.data[0].profile_picture', typeof data.data[0].profile_picture);
-    let options = {
+    const options = {
       Bucket: config.s3.bucketname,
       Key: key
     };
-    s3.getObject(options, (err, data) => {
+    s3.getObject(options, (err, s3Data) => {
       if (err) {
         console.log('Error retrieving file from bucket!', err);
         try {
           image = fs.readFileSync('./public/images/anon.png');
           res.send(image);
-        } catch (err) {
-          console.log('Could not read image!', err);
+        } catch (errReadFileSync) {
+          console.log('Could not read image!', errReadFileSync);
         }
       } else {
-        console.log('success! Data retrieveed:', data.Body);
-        image = data.Body;
+        console.log('success! Data retrieveed:', s3Data.Body);
+        image = s3Data.Body;
         res.send(image);
       }
     });
-  }).catch(err => {
+  }).catch((err) => {
     console.log('Error finding agent!', err);
     try {
       image = fs.readFileSync('./public/images/anon.png');
       res.send(image);
-    } catch (err) {
-      console.log('Could not read image!', err);
+    } catch (errImage) {
+      console.log('Could not read image!', errImage);
     }
   });
 });
 
 router.get('/profilePicPoll', (req, res) => {
-  let profilePicFlag = { profilePicExists: false };
+  const profilePicFlag = { profilePicExists: false };
 
   getAgent(req.session.user.username).then((data) => {
     if (data.data[0].profile_picture && data.data[0].profile_picture.length > 1) {
@@ -784,37 +801,37 @@ router.post('/videomailupload', consumerRestrict, (req, res) => { // add restric
     if (err) {
       console.error(err);
       res.writeHead(200, {
-        'content-type': 'text/plain',
+        'content-type': 'text/plain'
       });
       res.write('an error occurred');
     } else {
-      let filepath = files.file.filepath;
-      fs.readFile(filepath, function (err, fileData) {
+      const { filepath } = files.file;
+      fs.readFile(filepath, (_err, fileData) => {
         if (files.file.mimetype === 'video/webm') {
-          let uploadParams = { Bucket: utils.getConfigVal(config.s3.bucketname), Key: files.file.newFilename + '.webm', Body: '' };
+          const uploadParams = { Bucket: utils.getConfigVal(config.s3.bucketname), Key: `${files.file.newFilename}.webm`, Body: '' };
           uploadParams.Body = fileData;
-          s3.upload(uploadParams, function (err) {
-            if (err) {
-              console.log('Error!:', err);
+          s3.upload(uploadParams, (errS3Upload) => {
+            if (errS3Upload) {
+              console.log('Error!:', errS3Upload);
               return;
             }
             try {
               fs.unlinkSync(filepath);
-            } catch (err) {
-              console.error(err);
+            } catch (errUnlinkSync) {
+              console.error(errUnlinkSync);
             }
 
             request({
               method: 'POST',
-              url: 'https://' + utils.getConfigVal(config.servers.main_private_ip) + ':' + utils.getConfigVal(config.app_ports.mserver) + '/UploadVideomail',
+              url: `https://${utils.getConfigVal(config.servers.main_private_ip)}:${utils.getConfigVal(config.app_ports.mserver)}/UploadVideomail`,
               rejectUnauthorized: false,
               form: {
                 ext: '88888', //
                 duration: Math.floor(fields.duration),
                 phoneNumber: req.session.user.phone,
-                filename: files.file.newFilename + '.webm'
-              },
-            }, function (error, response, data) {
+                filename: `${files.file.newFilename}.webm`
+              }
+            }, (error, _response, _data) => {
               if (error) {
                 console.log('Error', error);
                 console.log('Could not upload:', new Date());
